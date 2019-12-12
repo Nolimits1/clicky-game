95180b8e47e67346ee1c1e48d267eed2a1844706
Emilio Valladares <vallae.wumbo@gmail.com>
2019-12-11 19:42:58 -0800

    my first app with react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import Header from './components/header/Header'
+
+function App() {
+  return (
+    <div className="App">
+      <Header headerName = "Clicky Game"/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function header({headerName}){
+    return(
+        <div>
+            <h1>{headerName}</h1>
+        </div>
+    );
+}
+
+export default header;
